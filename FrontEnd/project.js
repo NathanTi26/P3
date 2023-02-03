923223334a4a7b9224dd23710360500343e88130
NathanTi26 <nathan.titeca26@gmail.com>
2023-02-03 07:05:31 +0100

    p3

---
+++ FrontEnd/project.js
@@ -0,0 +1,184 @@
+
+
+const reponse = await fetch('http://localhost:5678/api/works');
+const projects = await reponse.json();
+
+
+function genererProject(projectfiltres)
+
+{
+
+    for (let i = 0; i < projectfiltres.length; i++) {
+
+
+
+
+    const article = projectfiltres[i] ;
+
+
+    const sectionFiches = document.querySelector(".gallery");
+
+    const projectElement = document.createElement("figure");
+
+    const imageElement = document.createElement("img") ;   
+    imageElement.crossOrigin = "anonymous";
+    imageElement.src = article.imageUrl;
+
+    const nomElement = document.createElement("figcaption");
+    nomElement.innerText = article.title;
+
+    sectionFiches.appendChild(projectElement);
+
+    projectElement.appendChild(imageElement);
+    projectElement.appendChild(nomElement);
+
+}
+ }
+
+ genererProject(projects); // Premier affichage de la page
+
+
+// Bouton filtre Objets
+
+ const boutonObjects = document.getElementById("btn-filtrer"); 
+
+ boutonObjects.addEventListener("click", function () {
+    const projetcsFiltre = projects.filter(function (project) {
+        return project.categoryId === 1;
+    });
+   console.log(projetcsFiltre)
+
+   document.querySelector(".gallery").innerHTML = "";
+
+   genererProject(projetcsFiltre);
+
+});
+
+// Bouton filtre Appartement
+
+
+
+const boutonFlat = document.getElementById("btn-filtrer2"); 
+
+boutonFlat.addEventListener("click", function () {
+   const projetcsFiltre2 = projects.filter(function (project) {
+    return project.categoryId === 2;
+   });
+
+
+
+  console.log(projetcsFiltre2)
+
+  document.querySelector(".gallery").innerHTML = "";
+
+  genererProject(projetcsFiltre2);
+  
+});
+
+// Bouton filtre Hotels & Restaurants
+
+
+
+const boutonHotel = document.getElementById("btn-filtrer3"); 
+
+boutonHotel.addEventListener("click", function () {
+   const projetcsFiltre3 = projects.filter(function (project) {
+    return project.categoryId === 3;
+   });
+
+
+
+  console.log(projetcsFiltre3)
+
+  document.querySelector(".gallery").innerHTML = "";
+
+  genererProject(projetcsFiltre3);
+  
+});
+
+// Bouton filtre Tous
+
+const boutonAll = document.getElementById("btn-filtrer4"); 
+
+boutonAll.addEventListener("click", function () {
+   const projetcsFiltre4 = projects.filter(function (project) {
+    return projects
+   });
+
+  console.log(projetcsFiltre4)
+
+  document.querySelector(".gallery").innerHTML = "";
+
+  genererProject(projetcsFiltre4);
+});
+
+
+
+
+
+const displayform = document.getElementById('displayform')
+
+
+displayform.addEventListener('click', showForm);
+
+function showForm(){
+  document.querySelector('.hiddensection').classList.toggle('hidden');
+  document.querySelector('.connexion-box').classList.toggle('visible');
+}
+
+
+
+
+// Box connexion
+
+const boutonLog = document.getElementById("btn-log");
+
+boutonLog.addEventListener("click", async function(e) {
+
+  e.preventDefault();
+  
+  let user = {
+
+    email: document.getElementsByName("email"),
+    password: document.getElementsByName("password")
+
+  };
+  
+  
+  let res = await fetch('http://localhost:5678/api/users/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify(user),
+  });
+  
+  let result = await res.json();
+
+  console.log(result);
+
+  localStorage.set('token',result);
+
+})
+
+
+// Gestion couleur des filtres actifs
+
+
+
+const filter = document.querySelectorAll('.btn-filtre')
+
+for ( let allFilter of filter){
+
+  allFilter.addEventListener('click', function(){
+
+for (let filterRemove of filter){
+
+filterRemove.classList.remove('active')
+
+}
+
+allFilter.classList.add('active')
+})
+}
+
